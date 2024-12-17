b8335f69952396edf1cbd8e665b7db91f1d08d98
Jakub Stelmasiak <stelmon3@gmail.com>
2024-12-17 19:47:05 +0100

    fix

--- external_file.py
+++ external_file.py
@@ -1,38 +1,40 @@
 from pathlib import Path
 
-
-# Skanowanie folderu i analiza plików
-def analyze_and_save_data():
-    # 1. Wprowadzenie ścieżki
-    path = Path(input("Wprowadź ścieżkę folderu do analizy: "))
-
-    # Sprawdzenie, czy podana ścieżka jest folderem
-    if not path.is_dir():
-        print("Podana ścieżka nie jest folderem.")
-        return
-
-    # 2. Zbieranie danych o plikach
-    analysis_data = []
-
-    # Iterowanie przez pliki w folderze
-    for file in path.iterdir():
-        if file.is_file():
-            file_name = file.name
-            word_count = len(file_name.split())  # Liczenie słów w nazwie pliku
-            char_count = len(file_name)  # Liczenie znaków w nazwie pliku
-            analysis_data.append((file_name, word_count, char_count))
-
-    # 3. Zapis danych do pliku
-    output_file = "file_analysis.txt"
-
-    # Zapis danych do pliku
-    with open(output_file, "w", encoding="utf-8") as f:
-        for file_name, word_count, char_count in analysis_data:
-            f.write(f"Plik: {file_name}, Słowa: {word_count}, Znaki: {char_count}\n")
-
-    # Potwierdzenie zapisania danych
-    print(f"Dane zostały zapisane do pliku {output_file}")
+#SKANOWANIE WYBRANEJ SCIEZKI
+def folders_list():
+    path = Path(input("Enter the path that you wanna scan: "))
+    folders = []
+    for i, element in enumerate(path.iterdir()):
+        if element.is_dir():
+            print(i + 1, element.name)
+            folders.append(element.name)
+    return folders
 
 
-# Uruchomienie funkcji
-analyze_and_save_data()
+a = folders_list()
+print(a[0])
+
+# SZYFROWANIE
+def cezar_code(name_to_encrypt, key):
+    output = ""
+    for char in name_to_encrypt:
+        if char.isalpha():
+            digit_move = 65 if char.isupper() else 97
+            output += chr((ord(char) - digit_move + key) % 26 + digit_move)
+        else:
+            output += char
+    return output
+
+
+def cezara_decrypt(name_to_encrypt, key):
+    return cezar_code(name_to_encrypt, -key)
+
+
+name_to_encrypt = "Witaj w świecie kryptografii!"
+key = 3
+
+encrypted = cezar_code(name_to_encrypt, key)
+print("Zaszyfrowany tekst:", encrypted)
+
+decrypted = cezara_decrypt(encrypted, key)
+print("Odszyfrowany tekst:", decrypted)
